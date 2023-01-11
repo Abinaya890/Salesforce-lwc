73eef2394968cb39878c105d7b13238a50c24668
Abinaya890 <abinayachandrakumar27@gmail.com>
2023-01-11 19:35:48 +0530

    calc

--- force-app/main/default/lwc/lwcCalculator/lwcCalculator.js
+++ force-app/main/default/lwc/lwcCalculator/lwcCalculator.js
@@ -3,9 +3,29 @@ export default class LwcCalculator extends LightningElement {
 data1;
 data2;
 result;
+handleClick(event){
+    const button = event.target.name;
+    if(button=='add'){
+        this.result= parseInt(this.data1) + parseInt(this.data2);
+    }
+    else if(button=='subtract'){
+       this.result= parseInt(this.data1) - parseInt(this.data2);
+    }
+    else if(button=='multiplication'){
+       this.result= parseInt(this.data1) * parseInt(this.data2);
+
+    }
+    else if(button=='division'){
+       this.result= parseInt(this.data1) / parseInt(this.data2);
+    }
+}
 handlechange(event){
-    if(event.target.name== 'input1'){ 
-        
-    }   this.data1=event.target.value;
+    const val = event.target.name;
+    if(val=='input1'){
+     this.data1=event.target.value;
+    }
+    else if(val=='input2'){
+     this.data2=event.target.value;
+    }
 }
 }
