2bf927aa0a45ce8c929455cadbf36c0d900ae39a
Abinaya890 <abinayachandrakumar27@gmail.com>
2023-01-17 19:54:20 +0530

    fireach

---
+++ force-app/main/default/lwc/foreachIterate/foreachIterate.js
@@ -0,0 +1,9 @@
+import { LightningElement } from 'lwc';
+export default class ForeachIterate extends LightningElement 
+{
+contacts=[
+    {Id:1, Name:'abi', Title:'salesforce' },
+    {Id:2, Name:'prasanth', Title:'sales' },
+    {Id:3, Name:'dhanush', Title:'force' }
+ ];
+}
