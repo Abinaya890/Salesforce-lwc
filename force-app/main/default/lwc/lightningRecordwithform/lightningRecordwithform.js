d1bc78b4045ead9b78aaa210ae143fce384dbeac
Abinaya890 <abinayachandrakumar27@gmail.com>
2023-01-25 19:57:30 +0530

    record

---
+++ force-app/main/default/lwc/lightningRecordwithform/lightningRecordwithform.js
@@ -0,0 +1,25 @@
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { NavigationMixin } from 'lightning/navigation';
+import { LightningElement } from 'lwc';
+export default class LightningRecordwithFormLayout extends NavigationMixin(LightningElement){
+
+handleSuccess(event)
+    {
+        const evt = new ShowToastEvent({ 
+        title: "Account Created",
+        message: "Record Id -"+event.detail.id,
+        variant: "success"
+        });
+        this.dispatchEvent(evt);
+
+        this[NavigationMixin.Navigate]
+        ({
+   
+             type: 'standard__recordPage',
+             attributes:{
+                recordId : event.detail.id,
+                objectApiName:'Account',
+                actionName : 'view'
+            },
+         });
+}}
